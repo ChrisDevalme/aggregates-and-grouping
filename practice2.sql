5a7f0ce5b150ff6ec47805f0d9de096805338286
ChrisDevalme <142542375+ChrisDevalme@users.noreply.github.com>
2026-06-02 11:18:10 -0400

    Completed Assignment

---
+++ practice2.sql
@@ -0,0 +1,40 @@
+-- Question 1:
+Select count(*) 
+from northwind.Suppliers;
+
+-- Question 2:
+SELECT sum(round(Salary)) 
+FROM northwind.Employees;
+
+-- Question 3:
+SELECT min(UnitPrice) 
+FROM northwind.Products;
+
+-- Question 4:
+SELECT AVG(round(UnitPrice)) 
+FROM northwind.Products;
+
+-- Question 5:
+SELECT max(UnitPrice) 
+FROM northwind.Products;
+
+-- Question 6:
+SELECT count(ProductName), SupplierID 
+FROM northwind.Products
+GROUP BY SupplierID;
+
+-- Question 7: 
+SELECT avg(round(UnitPrice)), CategoryID
+FROM northwind.Products
+GROUP BY CategoryID;
+
+-- Question 8:
+SELECT count(ProductName), SupplierID 
+FROM northwind.Products
+GROUP BY SupplierID 
+HAVING count(*) >= 5;
+
+-- Question 9:
+SELECT ProductID, ProductName, UnitPrice * UnitsInStock 
+FROM Products 
+Order by UnitPrice * UnitsInStock DESC, ProductName;
